0eaefc5dbcde967e366a7ae051b8ae2c94f6b73b
Montassar Braiek <montassar.braiek@etudiant-fst.utm.tn>
2025-10-19 20:57:44 +0100

    Add B_Make_it_Zigzag.cpp solution
    Implement solution for the Zigzag problem on CodeForces.

---
+++ CodeForces/B_Make_it_Zigzag.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+#define PI 3.1415926535897932384626433832795l
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+// ----------------------<MATH>---------------------------
+template <typename T>
+T gcd(T a, T b) { return (b ? __gcd(a, b) : a); }
+
+template <typename T>
+T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+bool isPrime(int n)
+{
+
+    // Since 1 is not prime return false
+    if (n == 1)
+        return false;
+
+    // Check divisiblity with all number in
+    // range [2, n - 1]
+    for (int i = 2; i < n; i++)
+    {
+
+        // If the n is divisible by i,
+        if (n % i == 0)
+            return false;
+    }
+
+    // If no divisor is found
+    return true;
+}
+
+bool cmp(pair<string, int>& a, 
+        pair<string, int>& b) 
+{ 
+    return a.second > b.second; 
+} 
+bool med(ll k,vector<ll>v,ll l){
+    int p=v.size()/2;
+
+    for (int i = p+1; i < v.size(); i++)
+    {
+        if (v[i]>k)
+        {
+            l-=v[i]-k;
+            if (l<0)
+            {
+                break;
+            }
+            
+        }
+        else 
+        {
+            break;
+        }
+        
+    }
+    return l>=0;
+    
+}
+void solve()
+{
+    ll n;
+    ll r;
+    ll l;
+    ll w;
+    ll c;
+    ll a;
+    ll b;
+    ll k;
+    ll o;
+    string s;
+    
+    vector<ll> v2;
+    vector<ll> v;
+    map<string,int>mp;
+    cin >> n;
+    for (int i = 0; i <n; i++)
+    {
+       cin>>o;
+        v.push_back(o);
+    }
+    a=0;
+    ll mx=-1;
+    for (int i = 0; i < n; i++)
+    {
+        mx=max(v[i],mx);
+        if ((i+1)%2==0)
+        {
+            v[i]=mx;
+        }
+    }
+    
+   for (int i = 0; i < n-1; i++)
+   {
+    if ((i+1)%2==0 && v[i]<=v[i+1])
+    {
+        a+=v[i+1]-v[i]+1;
+        v[i+1]=v[i]-1;
+    }
+   }
+   if (v[0]>=v[1])
+   {
+    a+=v[0]-v[1]+1;
+   }
+    cout<<a<<endl;
+    
+   
+
+     
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
