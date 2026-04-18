0d2517322b31f3050a975bf1670169ced89b9a4f
Montassar Braiek <montassar.braiek@etudiant-fst.utm.tn>
2026-04-18 11:38:23 +0100

    Add 1 accepted solutions

--- CodeForces/Codeforces_Round_299_(Div_2)_535/B_Tavas_and_SaDDas.cpp
+++ CodeForces/Codeforces_Round_299_(Div_2)_535/B_Tavas_and_SaDDas.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 template <typename A, typename B>
 ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
@@ -21,12 +21,12 @@ void dbg_out(Head H, Tail... T)
     dbg_out(T...);
 }
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-
+ 
 #define ll long long
 #define ld long double
 #define sza(x) ((int)x.size())
 #define all(a) (a).begin(), (a).end()
-
+ 
 #define PI 3.1415926535897932384626433832795l
 const int MAX_N = 1e5 + 5;
 const ll MOD = 1e9 + 7;
@@ -35,71 +35,159 @@ const ld EPS = 1e-9;
 // ----------------------<MATH>---------------------------
 template <typename T>
 T gcd(T a, T b) { return (b ? __gcd(a, b) : a); }
-
+ 
 template <typename T>
 T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
-bool isPrime(int n)
+const ll N=1e7;
+vector<bool>isPrime(N,true);vector<ll>p;
+void linear_sieve()
 {
-
-    // Since 1 is not prime return false
-    if (n == 1)
-        return false;
-
-    // Check divisiblity with all number in
-    // range [2, n - 1]
-    for (int i = 2; i < n; i++)
+    isPrime[0]=isPrime[1]=0;
+    for(ll i=2;i<N;i++)
     {
-
-        // If the n is divisible by i,
-        if (n % i == 0)
-            return false;
+        if(isPrime[i]==1) p.push_back(i);
+        for(ll o=0;o<p.size();o++)
+        {
+            if(i*p[o]>=N) break;
+            isPrime[i*p[o]]=0;
+            if(i%p[o]==0) break;
+        }
     }
-
-    // If no divisor is found
-    return true;
 }
-
-bool cmp(pair<string, int>& a, 
-        pair<string, int>& b) 
+ll powmod(ll x, ll y)
+{
+    ll res = 1;
+    x = x % MOD;
+    if (x == 0) return 0;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res*x) % MOD;
+        y = y>>1;
+        x = (x*x) % MOD;
+    }
+    return res;
+}
+vector<ll> fac,inv,finv;
+void init(ll n)
+{
+    fac.resize(n+1);
+    inv.resize(n+1);
+    finv.resize(n+1);
+    fac[0]=inv[0]=inv[1]=finv[0]=finv[1]=1;
+    for(ll i=1;i<=n;++i)fac[i]=fac[i-1]*i%MOD;
+    for(ll i=2;i<=n;++i)inv[i]=MOD-MOD/i*inv[MOD%i]%MOD;
+    for(ll i=2;i<=n;++i)finv[i]=finv[i-1]*inv[i]%MOD;
+}
+ll nCr(ll x,ll y)
+{
+    if(x<0||y>x||y<0)return(0);
+    return(fac[x]*finv[y]%MOD*finv[x-y]%MOD);
+}
+ll nPr(ll x,ll y)
+{
+    if(x<0||y>x||y<0)return 0;
+    return fac[x]*finv[x-y]%MOD;
+ }
+bool cmp(pair<int,bitset<2>>& a, 
+       pair<int,bitset<2>>& b) 
 { 
-    return a.second > b.second; 
+    return a.first > b.first; 
 } 
-
+bool med(ll k,vector<ll>v,ll l){
+    int p=v.size()/2;
+ 
+    for (int i = p+1; i < v.size(); i++)
+    {
+        if (v[i]>k)
+        {
+            l-=v[i]-k;
+            if (l<0)
+            {
+                break;
+            }
+            
+        }
+        else 
+        {
+            break;
+        }
+        
+    }
+    return l>=0;
     
-int ask(int x,int y){
-    cout<<"? "<<x<<' '<<y<<endl;
+}
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+int ask(int i,int j){
+    cout<<"? "<<i<<' '<<j<<endl;
     fflush(stdout);
     int r;
     cin>>r;
     return r;
 }
-set<ll>lo;
-void sad(ll n,ll b){
-    if (b>n)
-    {
-        return;
-    }
-    lo.insert(b);
-    sad(n,b*10+4);
-    sad(n,b*10+7);
-}
-void solve()
-{
-    ll n;
+ll m,n,k;vector<ll>v;vector<ll>v2;string res="";vector<ll>vv2; string s1;
     ll r;
     ll l;
     ll w;
-    ll c;
-    int a;
     ll b;
-    ll k;
     ll o;
-    vector<int>res;
-    vector<int>v;
+    ll c;
+    ll a;
+    ll d,e,f;
+bool ok(ll m){
+    ll s=0;
+    for (int i = 0; i < n; i++)
+    {
+        if (m*v[i]>v2[i])
+        {
+            s+=m*v[i]-v2[i];
+        }
+        if(s>k) return false;
+    }
+    return s<=k;
+}
+void rec(ll n){
+    if(n>=1e9) return;
+    v.push_back(n);
+    rec(n*10+4);
+    rec(n*10+7);
+}
+void solve() {
+    string s;
+    string s1;
+    v.clear();
+    v2.clear();
+    priority_queue<ll>pp;
+    set<ll>st;
+    vector<pair<ll,ll>>v1;
+    vector<ll>v0;
+    ll c=-1;
+    ll q;
+    unordered_map<ll,ll,custom_hash>mp;
+    unordered_map<ll,ll,custom_hash>mp1;
     cin>>n;
-    sad(n,0);
-    cout<<lo.size()-1;
-   
+    rec(0);
+    sort(all(v));
+    cout<<lower_bound(all(v),n)-v.begin()<<endl;
+    
+    
+    
+    
+    
+    
+    
+    
 }
 int main()
 {
@@ -107,10 +195,13 @@ int main()
     cin.tie(0);
     cout.tie(0);
     int tc = 1;
-   // cin >> tc;
+    //init(1e7);
+   // linear_sieve();
+    //cin >> tc;
     for (int t = 1; t <= tc; t++)
     {
-        // cout << "Case #" << t << ": ";
+        //memset(dp,-1,sizeof dp);
+         //cout << "Case #" << t << ": ";
         solve();
     }
 }
